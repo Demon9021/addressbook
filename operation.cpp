0476e8154d7dcc368a2f1c23b44c8b377717e9a3
infiniteTW <31698507+infiniteTW@users.noreply.github.com>
2019-07-26 10:04:43 +0800

    Update operation.cpp

--- operation.cpp
+++ operation.cpp
@@ -1,10 +1,13 @@
+#include"MySQLinterface.h"
 #include"Contacts.h"
 
 operation::operation() {  }
 operation::~operation() {  }
 
-void operation::Initial() {
-
+void operation::Initial(string& dbname,string &tablename) {
+	mysql.ConnectDB();
+	mysql.CreateDB(dbname);
+	mysql.CreateTable(tablename);
 }
 void operation::Change() {
 
@@ -13,8 +16,9 @@ void operation::Delete() {
 
 }
 void operation::Entry() {
-
+	mysql.WriteDataToDB()
 }
-void operation::Inquire() {
+void operation::Inquire(string&dbname,string&tablename,string &name) {
+	mysql.ReadDataFromDB( tablename, name);
 
 }
